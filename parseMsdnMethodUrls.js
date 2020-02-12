dd4d02382fff468f4cc7c4795bc4e67b9c5de286
kessler <yanivk@gmail.com>
2020-02-12 16:44:56 +0200

    refactor: parseMsdnMethodUrls.js

--- parseMsdnMethodUrls.js
+++ parseMsdnMethodUrls.js
@@ -1,10 +1,20 @@
-var xray = require('x-ray')
-var argv = require('minimist')(process.argv.slice(2))
+const scraper = require('./scraper')
+const REGISTRY_API_URL = 'https://docs.microsoft.com/en-us/previous-versions/windows/desktop/regprov/stdregprov'
 
-xray(argv.url || 'https://msdn.microsoft.com/en-us/library/aa393664(v=vs.85).aspx')
-	.select(['table#memberListMethods a[href]'])
-	.run(function(err, result) {
-		if (err) return console.error(err)
+async function main() {
+	const { shutdown, page, browser } = await scraper.launch({ url: REGISTRY_API_URL })
+	
+	await page.waitForSelector('table.table')
+	const elementHandles = await page.$$('table.table a');
+	const propertyJsHandles = await Promise.all(
+		elementHandles.map(handle => handle.getProperty('href'))
+	)
+	const urls = await Promise.all(
+		propertyJsHandles.map(handle => handle.jsonValue())
+	)
 
-		console.log(JSON.stringify(result))
-	})
+	console.log(JSON.stringify(urls))
+}
+
+main()
+
